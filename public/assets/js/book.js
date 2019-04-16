080b4addb694bc099adb11ac5526b9a7ca2808e1
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-04-16 15:09:32 +0530

    various changes

--- public/assets/js/book.js
+++ public/assets/js/book.js
@@ -35,13 +35,13 @@ function load_data()
         var ap = '<div class="col-md-4" style="margin-left:16px;">'+
         '<img src='+data[0].mcover+ ' data-bs-hover-animate="pulse" style="width:296px;height:359px;margin-left:-11px;">'+
         '<div class="dropdown" style="margin-top:-1px;">'+
-        '<button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="margin-left:2px;width:150px;margin-top:13px;">Theaters</button>'+
-            '<div class="dropdown-menu" role="menu">'+
-                '<a class="dropdown-item" role="presentation" href="#">First Item</a>'+
-                '<a class="dropdown-item" role="presentation" href="#">Second Item</a>'+
-                '<a class="dropdown-item" role="presentation" href="#">Third Item</a></div>'+
-       ' </div>'+
-        '<button class="btn btn-primary btn-block" style="background-color:rgb(0,123,255);width:95px;margin-top:-42px;margin-left:178px;padding:6px;">Seat Map</button>'+
+        '<select class="form-control" style="margin-left:2px;width:150px;margin-top:13px; background-color:rgb(0,123,255); color:white;">'+
+                '<option value="th1">First Item</option>'+
+                '<option value="th1">Second Item</option>'+
+                '<option value="th1">Second Item</option>'+ 
+            '</select>'+
+	'</div>'+
+        '<button class="btn btn-primary btn-block" style="background-color:rgb(0,123,255);width:95px;margin-top:-40px;margin-left:178px;padding:6px;">Seat Map</button>'+
         '<input placeholder="Enter Seat Number" style="font-size:18px;margin:2px;margin-bottom:0px;margin-top:42px;">'+
        ' <button class="btn btn-primary" style="width:114px;height:33px;margin:3px;padding:2px;font-size:22px;margin-left:244px;margin-top:-55px;background-color:rgb(0,123,255);">Book</button>'+
     '</div>'+
